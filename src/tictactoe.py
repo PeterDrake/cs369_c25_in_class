e6de87912f34700dd9059f02670a9235f318fbe7
Peter Drake <drake@lclark.edu>
2025-02-10 14:13:43 -0800

    Winner

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -7,4 +7,15 @@ def legal_moves(state):
     return [i for i in range(len(state)) if state[i] == '.']
 
 def winner(state):
-    return 1
+    lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+             [0, 3, 6], [1, 4, 7], [2, 5, 8],
+             [0, 4, 8], [2, 4, 6]]
+    for line in lines:
+        if state[line[0]] == state[line[1]] == state[line[2]]:
+            player = state[line[0]]
+            if player == 'X':
+                return 1
+            if player == 'O':
+                return -1
+    return 0
+
